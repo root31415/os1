ad1e0c5b9a6a37cb4e231f7c750b01bda42b87cf
root31415 <nezhad.armin@ut.ac.ir>
2020-03-04 08:17:29 -0500

    March4 16:46

--- 1/client.c
+++ 1/client.c
@@ -7,26 +7,42 @@
 
 int main(int argc, char const *argv[]){
 	
-	char server_resp[256];
+	char username[128];
+	char server_resp[32][32];
+	char input[1024];
+
+	read(1, username, sizeof(username));
 
 	int client_socket = socket(AF_INET, SOCK_STREAM, 0);
 
 	struct sockaddr_in address;
 	address.sin_family = AF_INET;
-	address.sin_port = htons(9003);
-	inet_pton(AF_INET, "127.0.0.1", &address.sin_addr);
+	address.sin_port = htons(9001);
+	//inet_pton(AF_INET, "127.0.0.1", &address.sin_addr);
+	address.sin_addr.s_addr = htonl(INADDR_ANY);
 
 	int connect_status;
 	if( (connect_status = connect(client_socket, (struct sockaddr *) &address, sizeof(address) ) ) < 0)
-		printf("Connecting ended up with an error");
+		write(1, "Connecting ended up with an error", sizeof("Connecting ended up with an error"));
 
-	
-	
-	recv(client_socket, &server_resp, sizeof(server_resp), 0);
-	//printf("Server response was %s", server_resp);
-	char output[256] = "Server response was : ";
-	strcat(output, server_resp); 
-	write(1, output, sizeof(output));
+	while(1){
+		read(1, input, sizeof(input));
+
+		if(strncmp(1, input, "log off", strlen("log off")) == 0){
+			//send(client_socket, input, sizeof(input));
+			break;
+		}
+
+		if(strncmp(input, "show groups", strlen("show groups")) == 0){
+			send(client_socket, input, sizeof(input), 0);
+			recv(client_socket, &server_resp, sizeof(server_resp), 0);
+			int k = 0;
+			while(strlen(server_resp[k]) > 0){ 
+				write(1, strcat(server_resp[k], "\n"), sizeof(strcat(server_resp[k],"\n")));
+				k++;
+			}
+		}
+	}
 	close(client_socket);
 
 }
--- 1/server.c
+++ 1/server.c
@@ -7,14 +7,18 @@
 
 int main(int argc, char const *argv[]){
 	
-	char server_msg[256] = "Hello my client!!!";
+	char group[128][32] = {};
+	char client_msg[128];
+	strcpy(group[0], "Group 0");
+	//strcpy(group[1], "Group 1000");
 
 	int serv_socket = socket(AF_INET, SOCK_STREAM, 0);
 
 	struct sockaddr_in address;
 	address.sin_family = AF_INET;
-	address.sin_port = htons(9003);
-	inet_pton(AF_INET, "127.0.0.2", &address.sin_addr);
+	address.sin_port = htons(9001);
+	//inet_pton(AF_INET, "127.0.0.1", &address.sin_addr);
+	address.sin_addr.s_addr = htonl(INADDR_ANY);
 
 	bind(serv_socket, (struct sockaddr *) &address, sizeof(address));
 
@@ -23,7 +27,12 @@ int main(int argc, char const *argv[]){
 
 		int client_socket = accept(serv_socket, NULL, NULL);
 
-		send(client_socket, server_msg, sizeof(server_msg), 0);
+		recv(client_socket, client_msg, sizeof(client_msg), 0);
+
+		if(strcmp(client_msg, "show groups")){
+			send(client_socket, group, sizeof(group), 0);
+		}
+		
 	}
 
 	close(serv_socket);
